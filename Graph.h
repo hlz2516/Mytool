081811bacb318f35409f130ff302f5481a383406
hlz2516 <40767020+hlz2516@users.noreply.github.com>
2019-05-06 21:03:34 +0800

    Add files via upload

--- Graph.h
+++ Graph.h
@@ -5,24 +5,13 @@
 #include <queue>
 #include "Mytool.h"
 #define MAX_CAPACITY 64
-#define PI 3.141592654
-//#define jt_rad 3 //箭头半径
-//#define rad 40
-//#define distance 10
-typedef enum {undiscovered,discovered,visited} Vstatus;
-typedef enum {undetermined,tree,cross,forward,backward} Etype;
-struct Vertex;
-//struct point {  //圆线上的点
-//	int x; int y;
-//	point(int _x,int _y):x(_x),y(_y) { }
-//};
-//point get_random_point(Vertex &v);
-//bool exists(Vertex v, int x, int y);
+typedef enum {undiscovered,discovered,visited} Vstatus;  //顶点状态
+//undiscovered 未被发现的		discovered 已被发现但还没出队列的  visited  已被发现并出了队列的
+typedef enum {undetermined,tree,cross,forward,backward} Etype;   //边状态
+//undetermined 还没被走过的边  tree 是属于树的一条边  cross 无法构成树的边
 //顶点类
 struct Vertex {
 	int data;//数据
-	//int x, y; //坐标
-	//int radius; //半径
 	int indegree, outdegree; //入度和出度
 	Vstatus status; //状态
 	int dtime, ftime;
@@ -30,16 +19,6 @@ struct Vertex {
 	Vertex(int _data = 0):data(_data),indegree(0),outdegree(0),
 		status(undiscovered),dtime(-1),ftime(-1),parent(-1) {}
 	//Vertex(int _x, int _y, int _data = 0);
-	void setdata(int _data) {
-		data = _data;
-		/*char t[10]; TCHAR tchar[10];
-		_gcvt_s(t, data, 3);
-		CharToTchar(t, tchar);
-		RECT r = { x - rad,y - rad,x + rad,y + rad };
-		drawtext(tchar, &r, DT_CENTER | DT_SINGLELINE | DT_VCENTER);*/
-	}
-	//void set_status(Vstatus _status);
-	//friend point get_random_point(Vertex &v);
 	bool operator==(Vertex &v)
 	{
 		if (this->data == v.data && this->indegree == v.indegree &&
@@ -49,131 +28,12 @@ struct Vertex {
 	}
 };
 
-//Vertex::Vertex(int _x, int _y, double _data):x(_x),y(_y),data(_data),radius(rad),
-//indegree(0),outdegree(0),status(undiscovered),dtime(-1),ftime(-1),parent(-1),priority(INT_MAX)
-//{
-//	setlinecolor(RED);
-//	circle(x, y, rad);
-//	char t[10]; TCHAR tchar[10];
-//	_gcvt_s(t, data, 3);
-//	CharToTchar(t, tchar);
-//	RECT r = { x - rad,y - rad,x + rad,y + rad };
-//	drawtext(tchar, &r, DT_CENTER | DT_SINGLELINE | DT_VCENTER);
-//}
-
-//Vertex::Vertex(double _data):data(_data),radius(rad),indegree(0),outdegree(0), //随机在窗口里生成顶点
-//status(undiscovered),dtime(-1),ftime(-1),parent(-1),priority(INT_MAX)
-//{
-//	start:x =rad + random(getwidth() - rad * 2);
-//	y = rad + random(getheight() - rad * 2);
-//	if (x < rad || y < rad) goto start;
-//    //检查有无顶点重叠，并且控制了两个顶点之间的最小距离，distance越大最小距离越大
-//	int cnt = 0, r = rad - 5;
-//	while (cnt < distance)
-//	{
-//		for (int i = x - r; i <= x + r; i++)
-//		{
-//			double yfang = r * r - (x - i)*(x - i);
-//			int y1 = y - sqrt(yfang), y2 = y + sqrt(yfang);
-//			if (getpixel(i, y1) != getpixel(i, y2))
-//				goto start;
-//		}
-//		r += 10;
-//		cnt++;
-//	}
-//
-//	setlinecolor(RED);
-//	circle(x, y, rad);
-//	char t[10]; TCHAR tchar[10];
-//	_gcvt_s(t, data, 3);
-//	CharToTchar(t, tchar);
-//	RECT rect = { x - rad,y - rad,x + rad,y + rad };
-//	drawtext(tchar, &rect, DT_CENTER | DT_SINGLELINE | DT_VCENTER);
-//}
-
-//void Vertex::set_status(Vstatus _status)
-//{
-//	if (_status == visited)
-//	{
-//		setlinecolor(GREEN);
-//		circle(x, y, rad);
-//		status = visited;
-//	}
-//	else if (_status == discovered)
-//	{
-//		setlinecolor(YELLOW);
-//		circle(x, y, rad);
-//		status = discovered;
-//	}
-//	else if (_status == undiscovered)
-//	{
-//		setlinecolor(RED);
-//		circle(x, y, rad);
-//		status = undiscovered;
-//	}
-//}
-
 struct Edge {
 	int data;
 	int weight;
 	Etype type;
 	Edge(int _data = 0, int _weight = 0):data(_data),weight(_weight),type(undetermined) { }
-	//void set_status(Etype);
-	//friend point get_random_point(Vertex &v);
 };
-
-//Edge::Edge(Vertex &_v1, Vertex &_v2, int _weight, double _data):v1(_v1),v2(_v2),weight(_weight),data(_data),type(undetermined)
-//{
-	//_v1.vp.push_back(get_random_point(_v1)); _v1.outdegree++;
-	//_v2.vp.push_back(get_random_point(_v2)); _v2.indegree++;
-	//POINT p1, p2;
-	//p1.x = _v1.vp.back().x; p1.y = _v1.vp.back().y;
-	//p2.x = _v2.vp.back().x; p2.y = _v2.vp.back().y;
-	//setlinecolor(WHITE);
-	//line(p1.x, p1.y, p2.x, p2.y);
-	//setfillcolor(WHITE);
-	//fillcircle(p2.x, p2.y, jt_rad);
-	//	//字符处理及权重表示
-	//char str[10]; itoa(weight, str, 10);
-	//TCHAR _tchar[10];CharToTchar(str, _tchar);
-	//RECT rect;
-	//if (abs(p1.x - p2.x) < 5)
-	//	rect = { p1.x,p1.y,p2.x + 10,p2.y };
-	//else if (abs(p1.y - p2.y) <5)
-	//	rect = { p1.x,p1.y,p2.x,p2.y + 10 };
-	//else 
-	//	rect = { p1.x,p1.y,p2.x,p2.y };
-	//drawtext(_tchar, &rect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
-//}
-
-//void Edge::set_status(Etype _type)
-//{//undetermined,tree,cross,forward,backward
-//	if (_type == undetermined)
-//	{
-//		setlinecolor(WHITE);
-//		type = undetermined;
-//	}
-//	else if (_type == tree)
-//	{
-//		setlinecolor(YELLOW);
-//		type = tree;
-//	}
-//	else if (_type == cross)
-//	{
-//		setlinecolor(BLUE);
-//		type = cross;
-//	}
-//	else if (_type == Etype::forward)
-//	{
-//		setlinecolor(CYAN);
-//		type = Etype::forward;
-//	}
-//	else if (_type == backward)
-//	{
-//		setlinecolor(BROWN);
-//		type = backward;
-//	}
-//}
 
 class GraphMatrix {
 private:
@@ -198,11 +58,21 @@ public:
 	bool exists(int i, int j) {  //边（i，j）是否存在
 		return ((i >= 0) && (i < n) && (j >= 0) && (j < n) && E[i][j] != (Edge*)NULL);
 	}
+	int& dTime(int i) { return V[i].dtime; } //返回vector<vertex>中指定索引为i的顶点的dtime
+	int& fTime(int i) { return V[i].ftime; }
+	Vstatus& status(int i) { return V[i].status; }
+	int& parent(int i) { return V[i].parent; }
 	int insert_v(int data);  //插入顶点，返回编号
 	int remove_v(int i);  //删除第i个顶点及其关联边
 	void insert_e(int data, int weight, int i, int j); //插入权重为w，从顶点i到j的边
 	int remove_e(int i, int j);
-	void bfs(Vertex &v1);
+	int nextNbr(int i,int j) {
+		while ((-1 < j) && (!exists(i, --j)));  //只要i与j顶点之间没有边或者j没有出界，循环继续
+		return j;                 //换句话说，只要在j逐渐减小到0的过程中找到了一条与i相连的边，就返回j
+	}
+	int firstNbr(int i) { return nextNbr(i, n); }
+	Etype& etype(int i, int j) { return E[i][j]->type; }
+	void bfs(int i,int& clock);//仅对单连通域有效，clock初始为0
 };
 
 int GraphMatrix::insert_v(int data)
@@ -251,77 +121,23 @@ int GraphMatrix::remove_e(int i, int j)
 	e--; V[i].outdegree--; V[j].indegree--;
 	return temp;
 }
-//point get_random_point(Vertex &v)
-//{
-//	int x, y;
-//	int i = 0;
-//	x = v.x - rad + random(2 * rad);
-//	int gene = random(2) ? 1 : -1;
-//	y = v.y + gene * sqrt(rad*rad - (v.x - x)*(v.x - x));
-//	if (v.vp.size() == 0)
-//	{
-//		point temp(x, y);
-//		return temp;
-//	}
-//	else {
-//		while (exists(v, x, y))
-//		{
-//			x = v.x - rad + random(2 * rad);
-//			int gene = random(2) ? 1 : -1;
-//			y = v.y + gene * sqrt(rad*rad - (v.x - x)*(v.x - x));
-//		}
-//		point temp(x, y);
-//		return temp;
-//	}
-//}
 
-//bool exists(Vertex v,int x, int y)//检验点是否存在重叠，如果重叠返回是
-//{
-//	for (int i = 0; i < v.vp.size(); i++)
-//		if (x > v.vp[i].x-5 && x<v.vp[i].x+5 &&  y > v.vp[i].y -5 && y < v.vp[i].y + 5)
-//			return true;
-//	return false;
-//}
-
-void GraphMatrix::bfs(Vertex &v1)
+void GraphMatrix::bfs(int i,int& clock)
 {
-	int index=0;
-	for (int i = 0; i < n; i++)
-		if (V[i].data == v1.data)
+	queue<int> Q;
+	status(i) = discovered; Q.push(i);  //将第i个顶点的状态设置为被发现，然后入队
+	while (!Q.empty()) {
+		int v = Q.front(); Q.pop();  //保存当前队列头的数据v然后pop
+		dTime(v) = ++clock;  //dtime代表第几轮
+		for (int u = firstNbr(v); u > -1; u = nextNbr(v, u)) //从v的第一个邻居顶点开始到最后一个
 		{
-			index = i;
-			break;
+			if (undiscovered == status(u))  //如果发现当前的邻居是没被发现的
+			{
+				status(u) = discovered; Q.push(u); //将其状态置为被发现的并入队
+				etype(u, v) = tree; parent(u) = v; //因为当前的边算树里的一条边，所以置为tree。将该邻居的父亲设为v
+			}
+			else etype(u, v) = cross; //其他的情况（一种是邻居被发现了但还在队列中，一种是邻居是已经出了队列的）都置为跨边
 		}
-
-	queue<Vertex> Q;
-	//v1.set_status(discovered); Q.push(v1);
-	while (!Q.empty())
-	{
-		Vertex v = Q.front();
-		Q.pop();
-		for (int i = 0; i < n; i++)
-			if (V[i].data == v.data)
-			{
-				index = i;
-				break;
-			}
-
-		for(int i=0;i<V.size();i++)
-			if (E[index][i] != NULL)
-			{
-				if (V[i].status == undiscovered)
-				{
-					//V[i].set_status(discovered);
-					Q.push(V[i]);
-					//E[index][i]->set_status(tree);
-					//V[i].parent =index;
-				}
-				else
-				{
-					//E[index][i]->set_status(cross);
-				}
-				Sleep(1000);
-			}
-		//v.set_status(visited);
+		status(v) = visited; 
 	}
 }
